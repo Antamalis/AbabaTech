2337c1b1f22dd6ecb67e5602c087df96cfbd3616
Mazvydas <mjaniskevicius@gmail.com>
2023-09-06 19:00:06 +0300

    Database setup

---
+++ server/models/movie.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    rating: { type: Number }
+}, { timestamps: true });
+
+module.exports = mongoose.model('movie', movieSchema);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 4000;
 const moviesRoutes = require('./routes/movies');
+const dbConfig = require('./config/database');
 
 const app = express();
 
@@ -17,6 +19,12 @@ app.get("/api", (req, res) => {
 
 app.use('/api/movies/', moviesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+mongoose.connect(dbConfig.database).then(() => {
+  console.log('Connected to MongoDB');
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+}).catch((error) => {
+  console.log(error);
 });
